e4da236dfa9df4e5fc7dbdfbe3f5ea17b0c8d4d5
Kunal Shivhare <KunalShivhare58@gmail.com>
2024-06-26 23:35:55 +0530

    feat:Cutom Fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,22 @@
-
-import { ScrollView, View,Text } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import * as Font from "expo-font";
+import { useEffect } from "react";
 export default function App() {
+  let [fontsLoaded, fontError] = Font.useFonts({
+    Inter: require("../src/fonts/inter/Inter.ttf"),
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <SafeAreaView className=" bg-black h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className="w-full h-full justify-center items-center px-4">
-          <Text className="text-white">
-            Earning Edge
-          </Text>
+          <Text className="text-white font-[Inter]">Earning Edge</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
